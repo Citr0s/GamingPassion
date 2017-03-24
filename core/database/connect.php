616c8f0ff0654e6e23beb225d602de146144c7d9
Milosz Dura <milosz.md@wp.pl>
2017-03-24 06:28:32 +0000

    correcting mysqli connect method, passing in database

--- core/database/connect.php
+++ core/database/connect.php
@@ -6,6 +6,6 @@
 	$mysql_user = DB_USERNAME;
 	$mysql_password = DB_PASSWORD;
 	
-	$connection = mysqli_connect($mysql_host, $mysql_user, $mysql_password) or die ('There was an error with loging into the database.');
+	$connection = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database) or die ('There was an error with logging into the database.');
 	
 	mysqli_select_db($connection, $mysql_database) or die ('There was an error with connecting to the database.');
