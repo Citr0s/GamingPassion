d72fc7b5d7ef93550acb14f377b85f391beb14f6
Milosz Dura <milosz.md@wp.pl>
2017-03-24 06:24:14 +0000

    correcting credentials path

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,10 +1,10 @@
 <?php
-    require_once '../../credentials.php';
+    require_once __DIR__ . '../../credentials.php';
 
-	$mysql_host = $DB_HOST;
-	$mysql_database = $DB_DATABASE;
-	$mysql_user = $DB_USER;
-	$mysql_password = $DB_PASSWORD;
+	$mysql_host = DB_HOST;
+	$mysql_database = DB_DATABASE;
+	$mysql_user = DB_USERNAME;
+	$mysql_password = DB_PASSWORD;
 	
 	$connection = mysqli_connect($mysql_host, $mysql_user, $mysql_password) or die ('There was an error with loging into the database.');
 	
