2a62039d09e0b838928048127e090e21917ebbd3
Milosz Dura <milosz.md@wp.pl>
2017-03-27 13:37:32 +0100

    fixing jquery selector

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@
 		$(function(){	
 			$("#slideshow > div:gt(0)").hide();
 			setInterval(function() { 
-			  $('#slideshoiw > div:first')
+			  $('#slideshow > div:first')
 			    .fadeOut(1000)
 			    .next()
 			    .fadeIn(1000)
