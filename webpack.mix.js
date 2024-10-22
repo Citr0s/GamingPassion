cd9f7999754bb577758ee0a8fa1ac034615e8685
Milosz Dura <miloszdura@gmail.com>
2024-10-22 15:15:08 +0100

    not processing urls

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ const {mix} = require('laravel-mix');
 
 mix.setPublicPath('')
     .js('js/app.js', 'dist/js')
-    .sass('sass/app.scss', 'dist/styles');
+    .sass('sass/app.scss', 'dist/styles')
+    .options({
+        processCssUrls: false
+    });
