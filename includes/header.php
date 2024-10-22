6345e2d11fa915f718dfe5ae57d8ab1b7b063a2a
Milosz Dura <miloszdura@gmail.com>
2024-10-22 15:37:23 +0100

    reverting changes to home page

--- includes/header.php
+++ includes/header.php
@@ -1,96 +1,87 @@
-<?php
-include_once 'core/init.php';
-include_once 'includes/header.php';
-?>
-    <div id="container">
-        <?php
-        if (isset($_GET['success'])) {
-            echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="css/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged in</strong>.</td></tr></table></div>';
-        }
-        ?>
-        <?php
-        if (isset($_GET['logout'])) {
-            echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="css/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged out</strong>.</td></tr></table></div>';
-        }
-        ?>
-        <div id="slideshow">
-            <?php include_once 'includes/slideshow.php'; ?>
-        </div>
-        <div id="index-landing">
-            <div id="main-content">
-                <?php
-                if (isset($_GET['id'])) {
-                    include_once 'includes/single-post.php';
-                } else {
-                    $posts = $postService->getAll();
-                    if (sizeof($posts) === 0) {
-                        echo '<div class="empty_result">Currently there are no records in our database.</div>';
-                    }
-                    foreach ($posts as $post) {
-                        $ratingsResponse = $ratingService->getAllFor($post->id);
-                        ?>
-                        <div class="post">
-                            <div class="post-title"><a href="?id=<?php echo $post->id; ?>"><?php echo strtoupper($post->title); ?></a></div>
-                            <div class="post-social">
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <table>
-                                                <tr>
-                                                    <td>Rating:</td>
-                                                    <td>
-                                                        <?php
-                                                        if (loggedIn() && notVoted($connection, $post->id)) {
-                                                            ?>
-                                                            <div class="stars-bg">
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=1">
-                                                                    <div class="star-1"></div>
-                                                                </a>
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=2">
-                                                                    <div class="star-2"></div>
-                                                                </a>
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=3">
-                                                                    <div class="star-3"></div>
-                                                                </a>
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=4">
-                                                                    <div class="star-4"></div>
-                                                                </a>
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=5">
-                                                                    <div class="star-5"></div>
-                                                                </a>
-                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=6">
-                                                                    <div class="holder"></div>
-                                                                </a>
-                                                            </div>
-                                                            <?php
-                                                        } else {
-                                                            $ratingAverage = $ratingsResponse->average === null ? 0 : $ratingsResponse->average;
-                                                            ?>
-                                                            <img src="css/images/rating-<?php echo $ratingAverage; ?>-stars.png" title="<?php echo $ratingAverage; ?>/5"/>
-                                                            <?php
-                                                        }
-                                                        ?>
-                                                    </td>
-                                                    <td>(<?php echo sizeof($ratingsResponse->ratings); ?>)</td>
-                                                </tr>
-                                            </table>
-                                        </td>
-                                    </tr>
-                                </table>
-                            </div>
-                            <div class="post-info"><a href="profile.php?user=<?php echo $post->author; ?>"><?php echo $post->author; ?></a> at <?php echo date('H:i \o\n d F Y', $post->createdAt); ?></div>
-                            <div class="post-image"><a href="?id=<?php echo $post->id; ?>"><img src="<?php echo $post->thumbnail; ?>"/></a></div>
-                            <div class="post-sample"><?php echo $post->content; ?>
-                                <div class="read-full-article"><a href="?id=<?php echo $post->id; ?>" class="read-full-article-hover">Read full post ></a></div>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                }
-                ?>
-            </div>
-            <?php include_once 'includes/sidebar.php'; ?>
-        </div>
-    </div>
-<?php
-include_once 'includes/footer.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Gaming Passion | News, Reviews, Fun</title>
+	<link rel="stylesheet" href="/dist/css/app.css">
+	<link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
+	<link rel="icon" href="/assets/images/favicon.ico" type="image/x-icon" />
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css' />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-50985585-2', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+    <script src="/dist/js/app.js"></script>
+<body>
+	<div id="wrapper">
+		<div id="header">
+			<div class="container">
+				<div class="float-left">
+					<div style="width:0px;">
+						<a href="/"><img src="assets/images/logo-main.jpg" alt="gaming passion logo" /></a>
+					</div>
+				</div>
+				<div class="float-right">
+					<div id="login-area">
+						<span class="greeting">Welcome to our website!</span>
+						<span class="login-options">
+						<?php
+							if(isset($_SESSION['username'])){
+								echo '
+									<ul>
+										<li><a href="profile.php?user='.$user->username.'">My Account ('.$user->username.')</a></li>
+										<li><a href="badges.php">Badges</a></li>
+										<li><a href="logout.php">Logout</a></li>
+										<li><a href="messages.php">Private Messages</a></li>';
+
+										if(adminUser($connection) || moduser($connection)){
+											echo '
+												<li><a href="/dashboard">Dashbaord</a></li>
+											';
+										}
+
+								echo '
+									</ul>
+								';
+							}else{
+								echo '
+									<ul>
+										<li><a hrefg="login.php">My Account</a></li>
+										<li><a href="login.php">Badges</a></li>
+										<li><a href="login.php">Login</a></li>
+										<li><a href="register.php">Register</a></li>
+									</ul>
+								';
+							}
+						?>
+						</span>
+						<span class="search">
+							<form>
+								<input type="text" name="search" class="search-box" placeholder="Search" />
+								<button type="submit" class="button" style="margin-bottom:2px;"><i class="fa fa-search"></i></button>
+							</form>
+						</span>
+					</div>
+				</div>
+				<div class="holder"></div>
+			</div>
+		</div>
+		<div id="menu">
+			<div class="container">
+				<ul>
+					<li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+					<li><a href="news.php"><i class="fa fa-bolt"></i> News</a></li>
+					<li><a href="reviews.php"><i class="fa fa-bar-chart-o"></i> Reviews</a></li>
+					<li><a href="archive.php"><i class="fa fa-archive"></i> Archive</a></li>
+					<li><a href=""><i class="fa fa-comments-o"></i> Forum</a></li>
+				</ul>
+			</div>
+		</div>
--- index.php
+++ index.php
@@ -1,39 +1,96 @@
 <?php
-	include_once 'core/bootstrap.php';
-	include_once 'includes/header.php';
+include_once 'core/init.php';
+include_once 'includes/header.php';
 ?>
-		<div id="container">
-			<?php
-				if(isset($_GET['success'])){
-					echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="assets/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged in</strong>.</td></tr></table></div>';
-				}
-			?>
-			<?php
-				if(isset($_GET['logout'])){
-					echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="assets/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged out</strong>.</td></tr></table></div>';
-				}
-			?>
-			<div id="slideshow">
-			<?php include_once 'includes/slideshow.php'; ?>
-			</div>
-			<div id="index-landing">
-				<div id="main-content">
-				<?php
-					if(isset($_GET['id']))
-					{
-                        include_once 'includes/single-post.php';
-					}
-					else
-                    {
-                ?>
-                    <post-list></post-list>
+    <div id="container">
+        <?php
+        if (isset($_GET['success'])) {
+            echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="css/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged in</strong>.</td></tr></table></div>';
+        }
+        ?>
+        <?php
+        if (isset($_GET['logout'])) {
+            echo '<div class="green-message" style="margin-bottom:15px;"><table><tr><td style="padding-right:5px;"><img src="css/images/popup-info-icon.png" /></td><td>You have been successfully <strong>logged out</strong>.</td></tr></table></div>';
+        }
+        ?>
+        <div id="slideshow">
+            <?php include_once 'includes/slideshow.php'; ?>
+        </div>
+        <div id="index-landing">
+            <div id="main-content">
                 <?php
-					}
-				?>
-				</div>
-				<?php include_once 'includes/sidebar.php'; ?>
-			</div>
-		</div>
+                if (isset($_GET['id'])) {
+                    include_once 'includes/single-post.php';
+                } else {
+                    $posts = $postService->getAll();
+                    if (sizeof($posts) === 0) {
+                        echo '<div class="empty_result">Currently there are no records in our database.</div>';
+                    }
+                    foreach ($posts as $post) {
+                        $ratingsResponse = $ratingService->getAllFor($post->id);
+                        ?>
+                        <div class="post">
+                            <div class="post-title"><a href="?id=<?php echo $post->id; ?>"><?php echo strtoupper($post->title); ?></a></div>
+                            <div class="post-social">
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <table>
+                                                <tr>
+                                                    <td>Rating:</td>
+                                                    <td>
+                                                        <?php
+                                                        if (loggedIn() && notVoted($connection, $post->id)) {
+                                                            ?>
+                                                            <div class="stars-bg">
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=1">
+                                                                    <div class="star-1"></div>
+                                                                </a>
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=2">
+                                                                    <div class="star-2"></div>
+                                                                </a>
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=3">
+                                                                    <div class="star-3"></div>
+                                                                </a>
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=4">
+                                                                    <div class="star-4"></div>
+                                                                </a>
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=5">
+                                                                    <div class="star-5"></div>
+                                                                </a>
+                                                                <a href="vote.php?post_id=<?php echo $post->id; ?>&rating=6">
+                                                                    <div class="holder"></div>
+                                                                </a>
+                                                            </div>
+                                                            <?php
+                                                        } else {
+                                                            $ratingAverage = $ratingsResponse->average === null ? 0 : $ratingsResponse->average;
+                                                            ?>
+                                                            <img src="css/images/rating-<?php echo $ratingAverage; ?>-stars.png" title="<?php echo $ratingAverage; ?>/5"/>
+                                                            <?php
+                                                        }
+                                                        ?>
+                                                    </td>
+                                                    <td>(<?php echo sizeof($ratingsResponse->ratings); ?>)</td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="post-info"><a href="profile.php?user=<?php echo $post->author; ?>"><?php echo $post->author; ?></a> at <?php echo date('H:i \o\n d F Y', $post->createdAt); ?></div>
+                            <div class="post-image"><a href="?id=<?php echo $post->id; ?>"><img src="<?php echo $post->thumbnail; ?>"/></a></div>
+                            <div class="post-sample"><?php echo $post->content; ?>
+                                <div class="read-full-article"><a href="?id=<?php echo $post->id; ?>" class="read-full-article-hover">Read full post ></a></div>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
+            </div>
+            <?php include_once 'includes/sidebar.php'; ?>
+        </div>
+    </div>
 <?php
-	include_once 'includes/footer.php';
-?>
+include_once 'includes/footer.php';
